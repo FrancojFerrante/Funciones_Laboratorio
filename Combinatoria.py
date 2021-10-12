bff5783f7496b66a41e71674a10f149837fd1190
Franco Ferrante <francoferrante95@gmail.com>
2021-10-12 09:16:56 -0300

    Combinatoria

--- Combinatoria.py
+++ Combinatoria.py
@@ -10,7 +10,7 @@ Created on Wed Sep  1 11:33:17 2021
 import itertools
 
 
-def combinatoria_texto_no_orden(columnas_a_combinar):
+def combinatoria_texto_no_orden(columnas_a_combinar, cantidad = -1):
     """
     Crea las posibles combinaciones que pueden darse entre las columnas de interés.
     No tiene en cuenta el orden. Ej: ['Col1','Col2'] es igual a ['Col2','Col1']
@@ -29,10 +29,13 @@ def combinatoria_texto_no_orden(columnas_a_combinar):
 
     """
     all_combinations = []
-    for r in range(len(columnas_a_combinar) + 1):
-    
-        combinations_object = itertools.combinations(columnas_a_combinar, r)
-        combinations_list = list(combinations_object)
-        all_combinations += combinations_list
-    all_combinations.pop(0) # Elimino el primero porque está vacío
-    return all_combinations
+    if (cantidad == -1):
+        for r in range(len(columnas_a_combinar) + 1):
+            all_combinations += list(itertools.combinations(columnas_a_combinar, r))
+        all_combinations.pop(0) # Elimino el primero porque está vacío
+        return all_combinations
+    else:
+        for r in range(cantidad + 1):
+            all_combinations += list(itertools.combinations(columnas_a_combinar, r))
+        all_combinations.pop(0) # Elimino el primero porque está vacío
+        return all_combinations
